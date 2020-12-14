94857cc408422f7332403a716423228dc911a56f
senuLab <kondo.issei11@gmail.com>
2020-12-14 22:29:57 +0900

    scroll実装

--- scss/mixin/js/custom.js
+++ scss/mixin/js/custom.js
@@ -38,3 +38,45 @@ $(document).ready(function() {
 
 new WOW().init();
 
+$(window).on("scroll", function() {
+    if ($(this).scrollTop() > 100) {
+      $('.floating').show();
+    } else {
+      $('.floating').hide();
+    }
+});
+  
+  $('.floating').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+
+
+
+  $('.drawer-menu-item').click(function () {
+
+    let header = $(".header").innerHeight(); 
+    /* let speed = 300; */
+    let id = $(this).attr("href");
+    let position = $(id).offset().top - header;
+
+    $('body,html').animate({
+      scrollTop: position
+    }, 300);
+    return false;
+  });
+
+  $('.nav-item a').click(function () {
+
+    let header = $(".header").innerHeight(); 
+    /* let speed = 300; */
+    let id = $(this).attr("href");
+    let position = $(id).offset().top - header;
+
+    $('body,html').animate({
+      scrollTop: position
+    }, 300);
+    return false;
+  });
